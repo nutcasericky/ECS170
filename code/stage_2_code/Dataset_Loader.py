c0d0c637b177a8da0c671b5650755beae0dec596
Ethan Wong <Ethan.h.wong@outlook.com>
2026-04-19 17:23:28 -0700

    Update Dataset_Loader.py

--- code/stage_2_code/Dataset_Loader.py
+++ code/stage_2_code/Dataset_Loader.py
@@ -5,8 +5,8 @@ Concrete IO class for a specific dataset
 # Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
 # License: TBD
 
-from code.base_class.dataset import dataset
-
+from local_code.base_class.dataset import dataset
+import numpy as np
 
 class Dataset_Loader(dataset):
     data = None
@@ -15,16 +15,29 @@ class Dataset_Loader(dataset):
     
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
-    
+
     def load(self):
         print('loading data...')
         X = []
         y = []
-        f = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'r')
-        for line in f:
-            line = line.strip('\n')
-            elements = [int(i) for i in line.split(' ')]
-            X.append(elements[:-1])
-            y.append(elements[-1])
-        f.close()
+
+        with open(self.dataset_source_folder_path + self.dataset_source_file_name, 'r') as f:
+            for line in f:
+                elements = line.strip().split()
+
+                # try to safely parse
+                try:
+                    elements = [int(i) for i in elements]
+                except:
+                    continue
+
+                # skip only if clearly invalid
+                if len(elements) < 2:
+                    continue
+
+                # label first, rest features
+                y.append(elements[0])
+                X.append(elements[1:])
+
+        print("Loaded X shape:", np.array(X).shape)
         return {'X': X, 'y': y}
